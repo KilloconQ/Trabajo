1afddf7f34f910c59d101123a0ee8586c2ae48ca
KilloconQ <fjc1997v@gmail.com>
2021-05-10 20:50:07 -0500

    estilo y alineacion

--- php/index.php
+++ php/index.php
@@ -5,11 +5,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../stylesheet/style.css">
     <title>Inicio</title>
 </head>
 <body>
    <div class = "container-fluid cabecera" style = "height:200px">
-    <div></div>
+    <div class="container">    
+        <h1 class="titulo">BASS Cleaf</h1>
+        <p id="slogan">Tu boutique del bajo eléctrico</p> 
+    </div>
    </div> 
+   <nav class="navbar navbar-inverse navbar-static-top" role="navigation" data-spy="affix" data-offset="197">
+    <div class="container">
+        <div class="navbar-header" id="logo">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navegacion-fm" aria-expanded="false">
+                <span class="sr-only">Desplegar/Ocultar</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="index.php" class="navbar-brand"><i class="bi bi-vinyl-fill"></i></a>
+        </div>
+    </div>
+   </nav>
 </body>
 </html>
