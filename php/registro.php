c1d3193e5c6319a1212036fa24e6057270da747b
Fernando <fjc1997v@gmail.com>
2021-05-11 21:08:32 -0500

    jquery

--- php/registro.php
+++ php/registro.php
@@ -9,6 +9,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <title>Registro</title>
+    <script type="text/javascript" src="../js/jquery.js"></script>
 </head>
 <body>
     <div class="container">
@@ -16,7 +17,6 @@
         <div class="progress">
             <div class="progress-bar progress-bar-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
         </div>
-    
     <form method="post" action="" id="registration_form">
         <fieldset>
             <h2>Crea tu cuenta</h2>    
