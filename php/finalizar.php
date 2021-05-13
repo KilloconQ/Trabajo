cac84102f8f3ff8155921f24c129d55e6069d524
Fernando <fjc1997v@gmail.com>
2021-05-13 14:35:51 -0500

    se han cometido errores

--- php/finalizar.php
+++ php/finalizar.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="../stylesheet/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <title>Carro</title>
+    <title>Administrador</title>
     <script type="text/javascript" src="../js/jquery.js"></script>
     <link rel="shortcut icon" type="image/png" href="../images/favicon.ico">
 </head>
@@ -80,26 +80,87 @@
         </div>
     </div>
    </nav>
-   <section class = 'main container'>
-    <?php
-        if($_SERVER["REQUEST_METHOD"]=="GET"){
-            $nombre = $_GET['nombre']; 
-            echo "<article class='clearfix'>";
-            echo "<span><p>$nombre</p></li>";
-            echo "<form method=\"post\" action=\"#\">";
-            echo "<input type='number' name = 'cantidad' value = '1' >";
-            echo "<input type='hidden' name = 'nombre' value = $nombre>";
-            echo "<input type='submit' value='Comprar'>";
-            echo "</form></span>";
-            echo "</article>";
+      <!-- Inicio de sesión -->
+     <div class="container">
+          <h1 class="titulo">Iniciar sesión</h1>
+          <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?> method="post" id="login_form">
+          <div class="form-element">
+                  <label for="email">Email</label>
+                  <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
+              </div>
+              <div class="form-group">
+                  <label for="inputPassword">Contraseña</label>
+                  <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña" required>
+              </div>
+              <button type="submit" name="login" value="login" style="background-color:#333">Iniciar sesión</button>
+          </form>
+     </div>
+     <?php
+     
+     $user = 'root';
+   $password = 'root';
+   $db = 'my_db';
+   $host = 'localhost';
+   $port = 3306;
+   
+   $link = mysqli_init();
+   $success = mysqli_real_connect(
+      $link, 
+      $host, 
+      $user, 
+      $password, 
+      $db,
+      $port
+   );  
+
+       $email = $_POST['email'];
+   $contrasenna = $_POST['password'];
+
+if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+   }
+    $sql = mysqli_query($link,"SELECT email FROM usuario WHERE email = '$email'");
+    $sql2 = mysqli_query($link, "SELECT contrasenna FROM my_db.usuario WHERE contrasenna = '$contrasenna' ");
+    if ($SERVER["REQUEST_METHOD"] == "POST"){
+        if(mysqli_num_rows($sql) > 0){
+        if(mysqli_num_rows($sql2) > 0) {
+            echo "Existe el usuario";
+            session_start();
+
+        }else{
+            echo "No existe: primer else";
         }
-        $nombre = "";
-    ?> 
-            
 
+    } else {
+            echo "No existe: segundo else";
+    }
+    }
+       
+    
+    $cantidad = $_POST['cantidad'];
+    $nombre = $_POST['nombre'];
+    
+    $select = mysqli_query($link, "SELECT id_usuario FROM usuario WHERE email = '$email' AND contrasenna = '$contrasenna'");
+    $row1 = mysqli_fetch_array($select);
+    $id_usuario = $row1['id_usuario']; 
 
-    </section>
-   
-   
+    $sql3 = mysqli_query($link, "SELECT id_producto, cantidad FROM producto WHERE nombre_producto = '$nombre_producto'");
+    $row = mysqli_fetch_array($sql3);
+            $id_producto = $row['id_producto'];
+            $cantidad = $row['cantidad'] - $cantidad;          
+            $update = "UPDATE my_db.producto SET cantidad = $cantidad WHERE id_producto = $id_producto";
+
+            if(!mysqli_query($link, $update)) {die('Error: ' . mysqli_error($link));}
+    echo "<p style:'color:green' >Compra finalizada"; 
+
+    $insertar =  "INSERT INTO my_db.historial
+                    (id_usuario, id_producto)
+                    VALUES($id_usuario, $id_producto);";
+
+    mysqli_close($link);
+
+     
+     
+     ?>
 </body>
 </html>
