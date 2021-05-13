ebfa193763df8649ce03e11ba3775fb90f414ec5
Fernando <fjc1997v@gmail.com>
2021-05-12 21:04:00 -0500

    quizas un avance

--- php/recibe.php
+++ php/recibe.php
@@ -33,8 +33,28 @@
     $colonia= mysqli_real_escape_string($link, $_POST['colonia']);
     $ciudad= mysqli_real_escape_string($link, $_POST['ciudad']);
     $numero_tarjeta = mysqli_real_escape_string($link, $_POST['numero_tarjeta']) ;
+    $select = "SELECT my_db.id_usuario FROM my_db.usuario WHERE my_db.nombre_usuario = $nombre_usuario, my_db.email = $email";
+    $result = mysqli_query($link, $select);
+    $row = mysqli_fetch_all($result);
+    $id_numero = $row['id_numero'];
 
-   
-   echo "<p>$email, $contrasenna, $nombre_usuario, $fecha, $numero_tarjeta, $calle, $cp, $numero, $estado, $colonia, $numero_tarjeta</p>";
-//   header("Location: index.php");
+   $insert1 = "INSERT INTO my_db.usuario (nombre_usuario, email, contrasenna, nacimiento, numero_tarjeta) VALUES('$nombre_usuario', '$email', '$contrasenna', '$fecha', '$numero_tarjeta');";
+    
+    if(!mysqli_query($link, $insert1)) {
+       die('Error: ' . mysqli_error($link));
+    }
+
+    echo "1 record added";
+
+    $insert2 = "INSERT INTO my_db.direccion (id_usuario, calle, cp, numero, estado, colonia, ciudad) VALUES(, '', '', '', '', '', '');";
+    
+    if(!mysqli_query($link, $insert2)) {
+       die('Error: ' . mysqli_error($link));
+    }
+
+    echo "1 record added";
+    
+    mysqli_close($link);
+   //echo "<p>$email, $contrasenna, $nombre_usuario, $fecha, $numero_tarjeta, $calle, $cp, $numero, $estado, $colonia, $numero_tarjeta</p>";
+//   header("Location: index.php");*/
 ?>
